9fc00b063c1cde42491af7e7e705ae284dc82f6b
kanashvili <kanashvili@nomachine.com>
2022-02-17 03:05:12 +0400

    test

--- solution.go
+++ solution.go
@@ -3,6 +3,5 @@ package solution
 import "github.com/kyokomi/emoji/v2"
 
 func GetMessage() string {
-	res := emoji.Sprint("Hello :world_map:!")
-	return res
+	return emoji.Sprint("Hello :world_map:!")
 }
